fd1b406f1f9b8835e2d6b00fd051abe0056dd57b
Fruit <Fruit@158.108.136.37>
2016-11-07 00:40:17 +0700

    2DBox

--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -4,9 +4,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.PirateBay;
@@ -22,6 +32,9 @@ public class PlayScreen implements Screen{
 	private TiledMap map;
 	private OrthogonalTiledMapRenderer renderer;
 	
+	private World world;
+	private Box2DDebugRenderer b2dr;
+	
 	public PlayScreen(PirateBay game){
 		this.game = game;
 		gamecam = new OrthographicCamera();
@@ -31,6 +44,25 @@ public class PlayScreen implements Screen{
 		map = maploader.load("stage1.tmx");
 		renderer = new OrthogonalTiledMapRenderer(map);
 		gamecam.position.set(gamePort.getScreenWidth()/2, gamePort.getWorldHeight()/2, 0);
+		world = new World(new Vector2(0, 0), true);
+		b2dr = new Box2DDebugRenderer();
+		
+		BodyDef bdef = new BodyDef();
+		PolygonShape shape = new PolygonShape();
+		FixtureDef fdef = new FixtureDef();
+		Body body;
+		
+		for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+			Rectangle rect = ((RectangleMapObject) object).getRectangle();
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+			
+			body = world.createBody(bdef);
+			
+			shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+		}
 	}
 
 	@Override
@@ -59,6 +91,7 @@ public class PlayScreen implements Screen{
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		renderer.render();
+		b2dr.render(world, gamecam.combined);
 		
 		game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
 		hud.stage.draw();
