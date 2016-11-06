ce3250846665a5cee7edd9f5a8b1643774aa97da
Fruit <Fruit@158.108.136.37>
2016-11-06 11:50:20 +0700

    ViewPort

--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -3,28 +3,38 @@ package com.mygdx.game.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.PirateBay;
 
 public class PlayScreen implements Screen{
 	private PirateBay game;
 	Texture texture;
+	private OrthographicCamera gamecam;
+	private Viewport gamePort;
 	
 	public PlayScreen(PirateBay game){
 		this.game = game;
 		texture = new Texture("badlogic.jpg");
+		gamecam = new OrthographicCamera();
+		gamePort = new FitViewport(800, 480, gamecam);
 	}
 
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
 		
-	}
+	} 
 
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0,  0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		game.batch.setProjectionMatrix(gamecam.combined);
 		game.batch.begin();
 		game.batch.draw(texture, 0, 0);
 		game.batch.end();
@@ -32,7 +42,7 @@ public class PlayScreen implements Screen{
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
+		gamePort.update(width, height);
 		
 	}
 
